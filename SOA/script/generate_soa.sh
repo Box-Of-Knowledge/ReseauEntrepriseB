391c75eb6ad3e3baa88634bdb3c2c6f9406a9c4c
miguel-antoons <miguel.antoons@gmail.com>
2022-02-08 22:03:09 +0100

    modification du script de lancement

--- SOA/script/generate_soa.sh
+++ SOA/script/generate_soa.sh
@@ -4,7 +4,7 @@ cd ..
 mkdir /etc/bind/zones
 cp named.conf /etc/bind/named.conf
 cp config/named.conf.log /etc/bind/named.conf.log
-cp config/named.conf.local /etc/bind/named.conf.local
+cp config/named.conf.local /etc/bind/$1/named.conf.local
 cp config/named.conf.options /etc/bind/named.conf.options
 
 cp zones/db.$1 /etc/bind/zones/
