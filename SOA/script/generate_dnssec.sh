89f7571025b4e49bc781909386aaa8daebff9102
miguel-antoons <miguel.antoons@gmail.com>
2022-02-08 21:04:19 +0100

    modifs pour le script d'installation

--- SOA/script/generate_dnssec.sh
+++ SOA/script/generate_dnssec.sh
@@ -13,4 +13,6 @@ done
 # signer la zone l2-2.ephec-ti.be et les machines à l'intérieur
 dnssec-signzone -A -3 $(head -c 1000 /dev/random | sha1sum | cut -b 1-16) -N INCREMENT -o $1 -t db.$1
 
+exit 0
+
 # https://www.digitalocean.com/community/tutorials/how-to-setup-dnssec-on-an-authoritative-bind-dns-server-2
--- SOA/script/generate_soa.sh
+++ SOA/script/generate_soa.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 cd ..
 
+mkdir /etc/bind/zones
 cp named.conf /etc/bind/named.conf
 cp config/named.conf.log /etc/bind/named.conf.log
 cp config/named.conf.local /etc/bind/named.conf.local
 cp config/named.conf.options /etc/bind/named.conf.options
 
-cp zones/db.$1 /etc/bind/zones
-cp zones/db.$1 /etc/bind/zones
+cp zones/db.$1 /etc/bind/zones/
+cp zones/db.$1 /etc/bind/zones/
 
 chmod +x ./script/generate_dnssec.sh
 ./script/generate_dnssec.sh $1
+
+exit 0
