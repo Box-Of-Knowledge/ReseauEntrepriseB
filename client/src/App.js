515a4e6749626ce7468240971f13fbef3ef90931
Louis Carlier <63374771+CarlouMuhan@users.noreply.github.com>
2022-02-08 16:58:37 +0100

    frontendV1bis

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { Link } from "react-router-dom";
+import logo from "./img/only_logo.png";
 
 function App() {
   return (
     <div className="App">
-      <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+      <nav className="navbar navbar-expand-lg navbar-light fixed-top" style={{fontSize: "20px"}}>
         <div className="container">
-          <Link className="navbar-brand" to={"/"}>Box-Of-Knowledge</Link>
+          <Link className="navbar-brand" to={"/"}>
+            <img src={logo} style={{width: "47px", marginRight: "40px"}}/></Link>
           <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
             <ul className="navbar-nav ml-auto">
               <li className="nav-item">
--- client/src/components/home.js
+++ client/src/components/home.js
@@ -1,13 +1,18 @@
 import React, { Component } from "react";
+import logo from "./img/logo.png";
 export default class Home extends Component {
     render() {
         return (
-            <div style={{margin: "100px"}}>
+            <div style={{margin: "100px", marginTop: "none"}}>
+                <h1 style={{textAlign: "center"}}>Box-Of-Knowledge</h1><br></br>
                 <h3 style={{textAlign: "center"}}>
-                Bonjour ! Notre plateforme propose une solution professionnelle
+                Bonjour ! Notre plateforme propose une solution professionnelle<br></br>
                  pour un suivi de nombreuses formations en ligne.
                 </h3>
+                <div>
+                    
+                </div>
             </div>
         );
-    }
+    }   
 }
