40aee169d2f634fd734aa7a8f153cf7ee2f9cb67
Louis Carlier <63374771+CarlouMuhan@users.noreply.github.com>
2022-02-08 14:46:38 +0100

    FrontendV1
    Première partie de la création du frontend avec la navbar ainsi que les formulaires de connexion et d'inscription

---
+++ client/src/components/home.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+export default class Home extends Component {
+    render() {
+        return (
+            <div style={{margin: "100px"}}>
+                <h3 style={{textAlign: "center"}}>
+                Bonjour ! Notre plateforme propose une solution professionnelle
+                 pour un suivi de nombreuses formations en ligne.
+                </h3>
+            </div>
+        );
+    }
+}
