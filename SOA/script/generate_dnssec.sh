e08b54fb97920a07136d5f3477a8d4ecf55e1518
miguel-antoons <miguel.antoons@gmail.com>
2022-02-08 22:26:34 +0100

    changes to dnssec

--- SOA/script/generate_dnssec.sh
+++ SOA/script/generate_dnssec.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # génération des clés pour le dnssec
 cd /etc/bind/zones/
-dnssec-keygen -a NSEC3RSASHA1 -b 2048 -n ZONE $1
-dnssec-keygen -f KSK -a NSEC3RSASHA1 -b 4096 -n ZONE $1
+dnssec-keygen -a RSASHA256 -b 2048 -f KSK $1
+dnssec-keygen -a RSASHA256 -b 1280 -n ZONE $1
 
 # inclure les clés dans le fichier de zone
 for key in `ls K$1*.key`
@@ -11,7 +11,7 @@ echo "\$INCLUDE $key">> db.$1
 done
 
 # signer la zone l2-2.ephec-ti.be et les machines à l'intérieur
-dnssec-signzone -A -3 $(head -c 1000 /dev/random | sha1sum | cut -b 1-16) -N INCREMENT -o $1 -t db.$1
+dnssec-signzone -o $1 -N INCREMENT -t -k $1.ksk.key db.$1 $1.zsk.key
 
 exit 0
 
