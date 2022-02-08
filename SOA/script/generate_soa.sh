f1fe69d987dd369ff6cbf24174a9b5daaeed8442
miguel-antoons <miguel.antoons@gmail.com>
2022-02-08 20:35:12 +0100

    modifs pour le script d'installation

--- SOA/script/generate_soa.sh
+++ SOA/script/generate_soa.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 cd ..
 
-cp named.conf /etc/bind/
-cp config/named.conf.log /etc/bind
-cp config/named.conf.local /etc/bind
-cp config/named.conf.options /etc/bind
+cp named.conf /etc/bind/named.conf
+cp config/named.conf.log /etc/bind/named.conf.log
+cp config/named.conf.local /etc/bind/named.conf.local
+cp config/named.conf.options /etc/bind/named.conf.options
 
 cp zones/db.$1 /etc/bind/zones
 cp zones/db.$1 /etc/bind/zones
