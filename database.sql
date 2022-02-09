3ec1685f6c3ea3050fef5392c6d1e3aa3b650a1f
Simon Kinet <64420845+Nepsylon@users.noreply.github.com>
2022-02-09 11:20:22 +0100

    Update de la création de la base de données

--- database.sql
+++ database.sql
@@ -1,6 +1,44 @@
-CREATE DATABASE box;
+DROP TABLE IF EXISTS formation;
+DROP TABLE IF EXISTS former;
+DROP TABLE IF EXISTS student;
+DROP TABLE IF EXISTS commentary;
 
-CREATE TABLE users(
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255)
+CREATE TABLE formation(
+    formation_id SERIAL PRIMARY KEY,
+    title CHAR(255) not null,
+    data VARCHAR
+    );
+
+CREATE TABLE former(
+    former_id SERIAL PRIMARY KEY,
+    name CHAR(255) not null,
+    password CHAR(255) not null,
+    formation_id SERIAL,
+    CONSTRAINT fk_formation
+    FOREIGN KEY (formation_id)
+    REFERENCES formation(formation_id)
+    );
+
+CREATE TABLE student(
+    student_id SERIAL PRIMARY KEY,
+    name CHAR(255) not null,
+    password CHAR(255) not null,
+    formation_id SERIAL,
+    CONSTRAINT fk_formation
+    FOREIGN KEY (formation_id)
+    REFERENCES formation(formation_id)
+    );
+
+CREATE TABLE commentary(
+    comment_id SERIAL PRIMARY KEY,
+    description CHAR(255) not null,
+    formation_id SERIAL,
+    student_id SERIAL,
+    CONSTRAINT fk_formation
+    FOREIGN KEY (formation_id)
+    REFERENCES formation(formation_id),
+
+    CONSTRAINT fk_student
+    FOREIGN KEY (student_id)
+    REFERENCES student(student_id)
     );
