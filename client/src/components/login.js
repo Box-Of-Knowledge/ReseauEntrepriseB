76f4b2f722ea25e8176769df6c57c18e4c927175
cyrilGrandjean <64021617+cyrilGrandjean@users.noreply.github.com>
2022-02-11 02:35:20 +0100

    Revert "Fix error login"
    This reverts commit 24713c4ae9c67e51090538913c79536a6fbb1214.

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import './login.css';
 import userService from "./services/authUser.services";
 import formerService from "./services/authFormer.services";
@@ -6,20 +6,22 @@ import Form from "react-validation/build/form";
 import Input from "react-validation/build/input";
 import CheckButton from "react-validation/build/button";
 import Cookies from 'js-cookie';
-
 const required = value => {
     if (!value) {
-        return (
-            <div className="alert alert-danger" role="alert">
-                Veuillez remplir ce champ!
-            </div>
-        );
+      return (
+        <div className="alert alert-danger" role="alert">
+          Veuillez remplir ce champ!
+        </div>
+      );
     }
-};
+  };
 export default class Login extends Component {
 
     constructor(props) {
         super(props);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.onChangeName = this.onChangeName.bind(this);
+        this.onChangePassword = this.onChangePassword.bind(this);   
         this.state = {
             name: '',
             password: '',
@@ -30,10 +32,10 @@ export default class Login extends Component {
 
     onChangeName(e) {
         this.setState({
-            name: e.target.value
+          name: e.target.value
         });
     }
-
+    
     onChangePassword(e) {
         this.setState({
             password: e.target.value
@@ -41,101 +43,101 @@ export default class Login extends Component {
     }
 
     onChangeRadio = e => {
-        const {value} = e.target;
-
+        const { value } = e.target;
+    
         this.setState({
-            role: value
+          role: value
         });
-    };
+      };
 
-    handleSubmit = e => {
+   /*handleSubmit = e => {
         e.preventDefault();
         this.setState({message: ""})
         this.form.validateAll();
 
 
         if (this.checkBtn.context._errors.length === 0) {
-            if (this.state.role === 'Formateur') {
+            console.log(this.state)
+            if(this.state.role === 'Formateur'){
                 formerService.login(this.state.name, this.state.password)
-                    .then((res) => {
-                        //window.localStorage.setItem('accessToken', res.data.secure.accessToken);
-                        window.localStorage.setItem('refreshToken', res.data.secure.refreshToken);
-                        window.localStorage.setItem('former_id', res.data.former_id);
-                        window.location.href = '/formations';
-                    })
-            } else {
-                userService.login(this.state.name, this.state.password)
-                    .then((res) => {
-                        //window.localStorage.setItem('accessToken', res.data.secure.accessToken);
-                        window.localStorage.setItem('refreshToken', res.data.secure.refreshToken);
-                        window.localStorage.setItem('student_id', res.data.student_id);
-                        window.location.href = '/formations';
-                    })
+                .then((res) => {
+                    //window.localStorage.setItem('accessToken', res.data.secure.accessToken);
+                    window.localStorage.setItem('refreshToken', res.data.secure.refreshToken);
+                    window.localStorage.setItem('former_id', res.data.former_id);
+                    window.location.href='/formations';
+                })
             }
-        }
-        ;
-    };
+            else{
+                userService.login(this.state.name, this.state.password)
+                .then((res) => {
+                    //window.localStorage.setItem('accessToken', res.data.secure.accessToken);
+                    window.localStorage.setItem('refreshToken', res.data.secure.refreshToken);
+                    window.localStorage.setItem('student_id', res.data.student_id);
+                    window.location.href='/formations';
+                })
+            }
+      };
+    };*/
 
     render() {
 
-        return (
+         return (
             <div className="auth-wrapper">
-                <div className="auth-inner">
-                    <Form
-                        onSubmit={this.handleSubmit}
-                        ref={c => {
-                            this.form = c;
-                        }}>
-                        <h3>Connexion</h3>
+            <div className="auth-inner">
+            <Form
+            onSubmit={this.handleSubmit}
+            ref={c => {
+                this.form = c;
+              }}>
+                <h3>Connexion</h3>
 
-                        <div className="form-group">
-                            <label>Pseudo</label>
-                            <Input
-                                type="text"
-                                className="form-control"
-                                placeholder="Votre pseudo"
-                                name="name"
-                                value={this.state.name}
-                                onChange={this.onChangeName}
-                                validations={[required]}/>
-                        </div>
-
-                        <div className="form-group">
-                            <label>Mot de passe</label>
-                            <Input type="password"
-                                   className="form-control"
-                                   placeholder="Votre mot de passe"
-                                   name="password"
-                                   value={this.state.password}
-                                   onChange={this.onChangePassword}
-                                   validations={[required]}/>
-                        </div>
-
-                        <input type="hidden" name="_token"></input>
-                        <div>
-                            <input type="radio" value="Formateur" name="role" onChange={this.onChangeRadio}/> Formateur
-                            <input type="radio" value="Etudiant" name="role" onChange={this.onChangeRadio}
-                                   required/> Étudiant
-                        </div>
-                        <button
-                            className="btn btn-dark btn-bg btn-block login"
-                        >
-                            Se connecter
-                        </button>
-                        <p className="forpas-register text-right">
-                            Mot de passe perdu ? Récupérer le <a href="./ForgotPassword">ici</a>
-                        </p>
-                        <p className="forpas-register text-right">
-                            Pas encore de compte ? Inscrivez-vous <a href="./sign-up">ici</a>
-                        </p>
-                        <CheckButton
-                            style={{display: "none"}}
-                            ref={c => {
-                                this.checkBtn = c;
-                            }}/>
-                    </Form>
+                <div className="form-group">
+                    <label>Pseudo</label>
+                    <Input
+                    type="text"
+                    className="form-control"
+                    placeholder="Votre pseudo"
+                    name="name"
+                    value={this.state.name}
+                    onChange={this.onChangeName}
+                    validations={[required]}/>
                 </div>
+
+                <div className="form-group">
+                    <label>Mot de passe</label>
+                    <Input type="password"
+                    className="form-control"
+                    placeholder="Votre mot de passe"
+                    name="password"
+                    value={this.state.password}
+                    onChange={this.onChangePassword}
+                    validations={[required]}/>
+                </div>
+
+                <input type="hidden" name="_token" ></input>
+                <div>
+                    <input type="radio" value="Formateur" name="role" onChange={this.onChangeRadio}/> Formateur
+                    <input type="radio" value="Etudiant" name="role" onChange={this.onChangeRadio} required/> Étudiant
+                </div>
+                <button
+                className="btn btn-dark btn-bg btn-block login"
+                >
+                    Se connecter
+                </button>
+                <p className="forpas-register text-right">
+                    Mot de passe perdu ? Récupérer le <a href="./ForgotPassword">ici</a>
+                </p>
+                <p className="forpas-register text-right">
+                    Pas encore de compte ? Inscrivez-vous <a href="./sign-up">ici</a>
+                </p>
+                <CheckButton
+                    style={{ display: "none" }}
+                    ref={c => {
+                        this.checkBtn = c;
+                }}/>
+            </Form>
             </div>
+        </div>
         );
     }
 }
